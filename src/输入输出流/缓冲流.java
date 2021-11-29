b2015e9b58ef183cf4725b124ddd0e224feee531
qf <qf@DESKTOP-53AEVBO>
2021-11-30 00:04:19 +0800

    v1.2.5主要改动如下： +--------------------+  1.在输入输出流中新增了[缓冲流]和[随机流] +--------------------+

--- src/输入输出流/缓冲流.java
+++ src/输入输出流/缓冲流.java
@@ -6,22 +6,48 @@
 
 package 输入输出流;
 import java.io.*;
+import java.util.*;
 
 /**
+ * 1.BufferedReader 和 BufferedWriter类创建的对象称为缓冲输入流、输出流。
+2.二者增强了对文件的读取能力，可以每次读取一行数据，二FileReader类没有提供读取一行的方法。
+3.二者的构造方法分别是：BufferedReader(Reader in)和 BufferedWriter(Writer out)
+4.BufferedWriter有一个独特的写入回车符的方法：newLine()
  * @author  qf
  * @date    2021-11-28 23:53:05
  * @version V1.0
  * @desc    
  */
 public class 缓冲流 {
-    public static void mian(String[]args){
-        
+    public static void main(String[]args){
+        File fRead = new File("english.txt");
+        File fWrite = new File("englishCount.txt");
+        try {
+            Writer out = new FileWriter(fWrite);
+            BufferedWriter bufferWrite = new BufferedWriter(out);
+            Reader in = new FileReader(fRead);
+            BufferedReader bufferRead = new BufferedReader(in);
+            String str = null;
+            while ((str=bufferRead.readLine())!=null) {
+              StringTokenizer fenxi = new StringTokenizer(str);
+              int count = fenxi.countTokens();
+              str=str+"句子中单词个数"+count;
+              bufferWrite.write(str);
+              bufferWrite.newLine();
+            }
+            bufferWrite.close();
+            out.close();
+            in = new FileReader(fWrite);
+            bufferRead = new BufferedReader(in);
+            String s = null;
+            System.out.println(fWrite.getName()+"内容:");
+           while((s=bufferRead.readLine())!=null){
+               System.out.println(s);
+           }
+           bufferRead.close();
+           in.close();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
     }
 }
-
-/*
-1.BufferedReader 和 BufferedWriter类创建的对象称为缓冲输入流、输出流。
-2.二者增强了对文件的读取能力，可以每次读取一行数据，二FileReader类没有提供读取一行的方法。
-3.二者的构造方法分别是：BufferedReader(Reader in)和 BufferedWriter(Writer out)
-4.BufferedWriter有一个独特的写入回车符的方法：newLine()
-*/
---
+++ src/输入输出流/随机流.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package 输入输出流;
+import java.io.*;
+
+
+/**
+ * randomAccessFilele类创建的流称为随机流，该流既不是
+ * InputStream类的子类，也不是OutputStram类的子类
+ * 但是该类既可以作为流的源，也可以作为流的目的地。
+ * 总的来说，当准备对一个文件进行读写操作时使用即可。
+ * 该类的常用方法：P293 表10.1
+ * --------------!! 需要注意------------
+ * ：readLine()方法在读取含有ASCII字符文件(如含有汉字的文件)时
+ * 会出现乱码现象，需要把该方法要读取的字符串用“iso-8859-1”编码从新编码
+ * 存放到byte数组中，然后用该机器的默认编码将该数组转换为字符串。
+ * @author  qf
+ * @date    2021-11-29 23:32:33
+ * @version V1.0
+ * @desc    
+ */
+public class 随机流 {
+    public static void main(String[]args){
+        RandomAccessFile inAndOut = null;
+        int data[] = {1,2,3,4,5,6,7,8,9,10};
+        try {
+            inAndOut = new RandomAccessFile("tom.dat", "rw"); // 该构造方法参数为:文件名，rw:可读写 or r:只读
+            for (int i = 0; i < data.length; i++) {
+                inAndOut.writeInt(data[i]);
+            }
+            for (long i = data.length-1; i>=0 ; i++) {
+                inAndOut.seek(i*4); // 一个int型数据占四个字节，inAndOut从文件的第36个字节读取后面的一个整数。
+                System.out.printf("\t%d", inAndOut.readInt()); // 每隔四个字节往前读取一个整数。
+            }
+            inAndOut.close(); // 关闭文件
+        } catch (Exception e) {
+            
+        }
+    }
+}
+/**
+ * 使用readLine读取文件
+ * @author qf
+ */
+class readLineShiYong{
+    public static void ShiYong(){
+        RandomAccessFile in =null;
+        try {
+            in = new RandomAccessFile("a.txt", "rw");
+            long length = in.length(); // 获取文件的长度
+            long position = 0; 
+            in.seek(position); // 将读取位置定位到文件的起始
+            while (position<length) {                
+                String str = in.readLine();
+                byte b[] = str.getBytes("iso-8859-1");
+                str = new String(b);
+                position = in.getFilePointer();
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+        }
+    }
+    
+}
